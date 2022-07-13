cb509f0870e722129602860d95cbd57b78b1b9eb
Genialqua <fred.aleokemalachi@gmail.com>
2022-07-13 04:04:26 +0000

    Recreating the printf function

--- print_buf.c
+++ print_buf.c
@@ -5,8 +5,7 @@
  * @buf: buffer pointer
  * @nbuf: number of bytes to print
  * Return: number of bytes printed.
-*/
-
+ */
 int print_buf(char *buf, unsigned int nbuf)
 {
 	return (write(1, buf, nbuf));
