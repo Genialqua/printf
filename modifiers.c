469c1722fc48fea8fd641c03ad96cc03a2893262
Ushecoder54 <thatwhite@gmail.com>
2022-07-13 08:19:08 -0700

    modifiers

--- modifiers.c
+++ modifiers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 unsigned int print_width(buffer_t *output, unsigned int printed,
-unsigned char flags, int wid);
+		unsigned char flags, int wid);
 unsigned int print_string_width(buffer_t *output,
-unsigned char flags, int wid, int prec, int size);
+		unsigned char flags, int wid, int prec, int size);
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-unsigned char flags, int wid);
+		unsigned char flags, int wid);
 
 /**
  * print_width - Stores leading spaces to a buffer for a width modifier.
@@ -18,16 +18,17 @@ unsigned char flags, int wid);
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_width(buffer_t *output, unsigned int printed,
-unsigned char flags, int wid)
+				unsigned char flags, int wid)
 {
 	unsigned int ret = 0;
 	char width = ' ';
 
 	if (NEG_FLAG == 0)
 	{
-	for (wid -= printed; wid > 0;)
-	ret += _memcpy(output, &width, 1);
+		for (wid -= printed; wid > 0;)
+			ret += _memcpy(output, &width, 1);
 	}
+
 	return (ret);
 }
 
@@ -42,17 +43,18 @@ unsigned char flags, int wid)
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_string_width(buffer_t *output,
-	unsigned char flags, int wid, int prec, int size)
+				unsigned char flags, int wid, int prec, int size)
 {
 	unsigned int ret = 0;
 	char width = ' ';
 
 	if (NEG_FLAG == 0)
 	{
-	wid -= (prec == -1) ? size : prec;
-	for (; wid > 0; wid--)
-	ret += _memcpy(output, &width, 1);
+		wid -= (prec == -1) ? size : prec;
+		for (; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
 	}
+
 	return (ret);
 }
 
@@ -60,22 +62,23 @@ unsigned int print_string_width(buffer_t *output,
  * print_neg_width - Stores trailing spaces to a buffer for a '-' flag.
  * @output: A buffer_t struct containing a character array.
  * @printed: The current number of bytes already stored to output
- *		for a given specifier.
+ *           for a given specifier.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
  *
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-unsigned char flags, int wid)
+				unsigned char flags, int wid)
 {
 	unsigned int ret = 0;
 	char width = ' ';
 
 	if (NEG_FLAG == 1)
 	{
-	for (wid -= printed; wid > 0; wid--)
-	ret += _memcpy(output, &width, 1);
+		for (wid -= printed; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
 	}
+
 	return (ret);
 }
